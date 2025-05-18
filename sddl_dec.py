83749463f4fe458d0ab09e3159c64aaf1f18233d
theubusu <80545678+theubusu@users.noreply.github.com>
2025-05-18 21:16:50 +0200

    Update 2.0

--- sddl_dec.py
+++ sddl_dec.py
@@ -1,34 +1,25 @@
-#!/usr/bin/python3
-
 from Crypto.Cipher import AES
 import os
-import sys
-import os.path
 import struct
 import zlib
-import shutil
+import sys
 from tqdm import tqdm
-import tarfile
-
-noTempDelete = False
 
 if len(sys.argv) < 2:
-    print("Usage: python sddl_dec.py <SDDL.SEC file> (-n)")
-    print("\nThe -n option will make the script not delete the temp folder.")
+    print("sddl_dec Tool Version 2.0")
+    print("Usage: python sddl_dec.py <SDDL.SEC file>")
     sys.exit(1)
 
 input_file = sys.argv[1]
 
-if len(sys.argv) > 2:
-    if sys.argv[2] == '-n':
-        noTempDelete = True
-
+# Load files
 with open('./crypto_key', 'rb') as f:
     key_buf = bytearray(f.read())
 with open(input_file, 'rb') as f:
     sddl_buf = bytearray(f.read())
-
-def decipher(b: bytearray):
+    
+# decipher the obfuscated key file
+def decipher_key_file(b: bytearray):
     assert len(b)<0x7f # (later complex logic is not reversed)
     acc = 0x388
     for i in range(len(b)):
@@ -36,7 +27,18 @@ def decipher(b: bytearray):
         b[i] ^= 0xff & (acc >> 8)
         acc = new_acc
         
-def decipher_2(s, len_):
+#AES-CBC decryption
+def decrypt_payload(b):
+    cip = AES.new(key, AES.MODE_CBC, iv)
+    return cip.decrypt(b)
+    
+#decryption function + remove PKCS#7 padding
+def decrypt_payload_unpad(b):
+    b1 = decrypt_payload(b)
+    return b1[:-b1[-1]]
+    
+# deciphering function
+def decipher(s, len_):
     v3 = 904
     out = bytearray(s)
     cnt = 0
@@ -64,117 +66,81 @@ def decipher_2(s, len_):
                 cnt += 1
         len_ -= true_len
         return out
+        
+print("sddl_dec Tool Version 2.0")
 
-
-decipher(key_buf)
-
+print("Deciphering key...")
+                
+# get key and iv from deciphered key
+decipher_key_file(key_buf)
 key = key_buf[:16]
 iv = key_buf[16:]
-
-print("sddl_dec Tool Version 1.0")
-
-# print(repr(key), repr(iv))
-
-def decrypt_payload(b):
-    cip = AES.new(key, AES.MODE_CBC, iv)
-    return cip.decrypt(b)
-def decrypt_payload_unpad(b): 
-    b1 = decrypt_payload(b)
-    return b1[:-b1[-1]]
-
-os.makedirs('sddldec_temp', exist_ok=True)
-    
-print("(1/3) Extracting...")
-
+#print(repr(key), repr(iv))
+        
+#initial offset = lenght of SDDL.SEC file header
 off = 0x20
-total_parts = -1
-while off < len(sddl_buf)-0x80:
-    hdr = decrypt_payload_unpad(sddl_buf[off:off+0x20])
-    file_name = hdr.split(b'\0')[0]
-    file_size = int(hdr[16:].decode())
-    file_content = sddl_buf[off+0x20:off+0x20+file_size]
-    off += 0x20 + file_size
-    print('Saved part: ', file_name, file_size)
-    if file_name.startswith(b'PEAKS.F'):
-        total_parts += 1
-    filenm = os.path.join('sddldec_temp', file_name.decode())
-    with open(filenm, 'wb') as f:
-        f.write(decrypt_payload_unpad(file_content))
 
-total_parts += 1
+#go through the file until 0x80 till the end
+with open("PEAKS.bin", "ab") as out_file:
+    while off < len(sddl_buf)-0x80:
+
+        #read header of entry with 0x20 lenght and decrypt it
+        hdr = decrypt_payload_unpad(sddl_buf[off:off+0x20])
     
-print("Total parts: " + str(total_parts))
+        #read null-terminated string - file name
+        file_name = hdr.split(b'\0')[0]
+    
+        #read from byte 16 - file size as ASCII
+        file_size = int(hdr[16:].decode())
+    
+        #read the files' content from buf with the read file size
+        file_content = sddl_buf[off+0x20:off+0x20+file_size]
+    
+        #advancing the offset for next file
+        off += 0x20 + file_size
+    
+        print('\nFound file: ' + file_name.decode("utf-8") + " Size: " + str(file_size))
+        filenm = file_name.decode()
+
+        #with open(filenm, 'wb') as f:
+        #    f.write(decrypt_payload_unpad(file_content))
         
-print("(2/3) Decompressing...")
+        #only extract PEAKS contents
+        if filenm.startswith("PEAKS"):
+    
+            decrypted_data = decrypt_payload_unpad(file_content)
+    
+            #size of data after decryptiom
+            data_size = struct.unpack('>I', decrypted_data[36:40])[0]
+    
+            # print("DATA SIZE: " + hex(data_size))
+            
+            #verify the data size
+            assert len(decrypted_data[48:]) == data_size
         
-for i in tqdm(range(total_parts)):
-    out_f = open(f"sddldec_temp/PEAKS.F{i:02}.out", "wb")
-    in_f = open(f"sddldec_temp/PEAKS.F{i:02}" , "rb")
-    in_f.read(0x20)
-    number = struct.unpack(">H", in_f.read(0x2))[0]
-    control = struct.unpack(">H", in_f.read(0x2))[0]
-    data_size = struct.unpack(">I", in_f.read(0x4))[0]
-    decrypt_size = struct.unpack(">I", in_f.read(0x4))[0]
-    unzlib_crc = struct.unpack(">I", in_f.read(0x4))[0]
-    data = in_f.read()
-    assert len(data) == data_size
-    decrypt_data = decipher_2(data, len(data))
-    if decrypt_data.startswith(b'\x01'):
-        out_f.write(decrypt_data)
-        print("\nSkipped uncompressed file " + str(i))
-    else:
-        out_f.write(zlib.decompress(decrypt_data, zlib.MAX_WBITS))
-    out_f.flush()
-    out_f.close()
-    in_f.close()
+            decipher_data = decipher(decrypted_data[48:], len(decrypted_data[48:]))
+        
+            if decipher_data.startswith(b'\x78\x9C'):
+                #decompress a compressed file
+                print("Decompressing...")
+                out_data = zlib.decompress(decipher_data, zlib.MAX_WBITS)      
+            else:
+                #file is not compressed, write raw data.
+                print("Skipping uncompressed file...")
+                out_data = decipher_data
+           
+            #read the offset to be saved from the file
+            file_offset = struct.unpack(">I", b'\x00' + out_data[6:9])[0]
+        
+            print("OFFSET: " + str(hex(file_offset)))
+                        
+            out_file.write(out_data[file_offset:])
+            
+            print('Appended file to PEAKS.bin: ', file_name)
+            
+        else:
+            with open(filenm, 'wb') as f:
+                f.write(decrypt_payload_unpad(file_content))
+              
+print("\nScript done. Saved extracted PEAKS data to PEAKS.bin")
 
-print("\nDetecting mode...")
-
-with open('sddldec_temp/PEAKS.F00.out', errors="ignore") as f:
-    if 'DLDATA_LIST.TXT' in f.read():
-        seek_val = 0x10E
-        print("Continue with tgz mode...")
-        mode = 1
-    else:
-        seek_val = 0xE
-        print("Continue with normal mode...")
-        mode = 2
-
-print("\n(3/3) Combining...")
-
-if mode == 1:
-    output_file='root.tgz'
-    start_range = 1
-else:
-   output_file='out.bin'
-   start_range = 0
-
-def merge():
-    combined_data = bytearray()
-
-    for i in tqdm(range(start_range, total_parts)):
-        filename = f"sddldec_temp/PEAKS.F{i:02d}.out"
-        try:
-            with open(filename, 'rb') as f:
-                f.seek(seek_val)
-                section = f.read()
-                combined_data.extend(section)
-        except FileNotFoundError:
-            print(f"File not found: {filename}")
-        except Exception as e:
-            print(f"Error processing {filename}: {e}")
-
-    with open(output_file, 'wb') as out_file:
-        out_file.write(combined_data)
-        print(f"\nExtracted data written to {output_file}")
-
-merge()
-
-if noTempDelete == False:
-    print("Deleting temp files...")
-    if os.path.exists('sddldec_temp'):
-        shutil.rmtree('sddldec_temp')
-    else:
-        print(f"Temp folder does not exist.")
-
-print("Script done.")
