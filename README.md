ef57f8273ed72f3cc59bc27d53cc24ca8757fb56
theubusu <80545678+theubusu@users.noreply.github.com>
2025-08-24 12:57:32 +0200

    Version 4.0
    Complete rewrite in Rust!
    Up to 25x speed increase!
    SO amazing!

--- README.md
+++ README.md
@@ -1,26 +1,28 @@
 # sddl_dec
-Tool for decrypting Panasonic TV SDDL.SEC files. New version 3.0  
-This Python script will decrypt and unpack the files from an SDDL.SEC firmware update package used on Panasonic TVs.  
-This script uses information about SDDL.SEC files found by NeSE Team for JustCTF 2022 and found [here](https://nese.team/posts/justctf). Thank you!  
-**Notice:** This script will not directly extract the contents of the firmware by itself! It only unpacks and decrypts the SDDL.SEC file. To inspect the output of the program, use a tool like [binwalk](github.com/ReFirmLabs/binwalk). To see what you can expect, read more below.
+Tool for decrypting Panasonic TV SDDL.SEC files. New version 4.0 rewritten in Rust for up to 25x the speed! 
+This tool will decrypt and unpack the files from an SDDL.SEC firmware update package used on Panasonic TVs.  
+**Notice:** The tool will not directly extract the contents of the firmware by itself! It only unpacks and decrypts the SDDL.SEC file. To inspect the output of the program, use a tool like [binwalk](github.com/ReFirmLabs/binwalk). To see what you can expect, read more below.
 ## Support
-The provided key file can extract SDDL.SEC files from TVs released in and after 2011. Older files seem to use a different format.
+The tool extract SDDL.SEC files from TVs released in and after 2011. Older files seem to use a different format.
+## Installation
+You can either:
+Download one of the prebuilt binaries from the Releases tab,  
+or build from source, by downloading the code and running `cargo build --release`. The binary will be saved in `target/release`.  
+  
+If you prefer to use the old python version, you can find it in the `python` branch.
 ## Usage
-Dependancies: `Crypto.Cipher`  
-`sddl_dec.py [-h] [-l] [-v] [-kt] input_file crypto_key_file [output_folder]`  
-`input_file` - The SDDL.SEC file to extract.  
-`crypto_key_file` - Crypto key to be used to decrypt the file.  
-`output_folder` - Folder to save extracted files to. Default: "out"  
-`-h` - Show help message.  
-`-l` - List contents of the file without extracting them.  
+`sddl_dec [OPTIONS] <INPUT_FILE> <OUTPUT_FOLDER>`  
+`<INPUT_FILE>` - The SDDL.SEC file to extract.  
+`<OUTPUT_FOLDER>` - Folder to save extracted files to.  
+`[OPTIONS]` - Can be:  
 `-v` - Verbose mode - print detailed information about extraction process.  
-`-kt` - Keep TXT files. (read more below)  
+`-k` - Keep TXT files. (read more below)  
+`-h` - Show help message.  
 ## About SDDL.SEC and the output of the program
 An SDDL.SEC file is an encrypted, ciphered and partially compressed archive that contains the firmware data for the TV.
-It has a 32 byte header, that mostly remains the same between files, but its structure is unknown, and a 128 or 256(in later files) byte footer, which is most likely a signature.  
 The main contents of the file can consist of:
 - SDIT.FDI - looks like it stores some kind of configuration data for different models, format is currently unknown.
-- A bunch of XXX.TXT files which contain the target and version of the update (These are skipped by default and printed to output instead because somtimes theres over 40 of them for some reason and they are not important. You can prevent that with the `-kt` option.)
+- A bunch of XXX.TXT files which contain the target and version of the update (These are skipped by default and printed to output instead because sometimes theres over 40 of them for some reason and they are not important. You can prevent that with the `-k` option.)
 - PEAKS.FXX Files - these are the main firmware data split into chunks, usually of 2/4MB of size - they are saved into a PEAKS.bin file, or in case of 2014-2018 files, the files embedded inside will be saved into a PEAKS folder.
 The output of the PEAKS files varies depending on the TV's platform, from my findings the structure is:
     - For 2011-2013 models, and some later "dumb" models, the output blob contains the FreeBSD kernel, UFS rootfs filesystem and a UFS filesystem (/usr) compressed by unknown method (This format is complicated and not yet fully known)
---
+++ main.rs
@@ -0,0 +1,267 @@
+use std::path::{Path, PathBuf};
+use std::fs::{self, File, OpenOptions};
+use std::io::{self, Read, Seek, SeekFrom, Write};
+
+use aes::Aes128;
+use clap::Parser;
+use flate2::read::ZlibDecoder;
+use cbc::{Decryptor, cipher::{block_padding::Pkcs7, BlockDecryptMut, KeyIvInit}};
+
+type Aes128CbcDec = Decryptor<Aes128>;
+
+/// Tool for decrypting and unpacking Panasonic TV SDDL.SEC update files. 
+#[derive(Parser, Debug)]
+struct Args {
+    /// Show detailed information about extraction process.
+    #[arg(short = 'v')]
+    verbose: bool,
+
+    /// Keep .TXT files
+    #[arg(short = 'k')]
+    keep_txt: bool,
+
+    input_file: String,
+    output_folder: String,
+}
+
+fn read_file(mut file: &File, offset: u64, size: usize) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    file.seek(SeekFrom::Start(offset))?;
+    let mut buffer = vec![0u8; size];
+    let _bytes_read = file.read(&mut buffer)?;
+
+    Ok(buffer)
+}
+
+fn decrypt(encrypted_data: &[u8], key: &[u8; 16], iv: &[u8; 16]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    let mut data = encrypted_data.to_vec();
+    let decryptor = Aes128CbcDec::new(key.into(), iv.into());
+    let decrypted = decryptor.decrypt_padded_mut::<Pkcs7>(&mut data)
+        .map_err(|e| format!("!!Decryption error!!: {:?}", e))?;
+    
+    Ok(decrypted.to_vec())
+}
+
+//ported from original from https://nese.team/posts/justctf/
+fn decipher(s: &[u8], len_: usize) -> Vec<u8> {
+    let mut v3: u32 = 904;
+    let mut out = s.to_vec();
+    let mut cnt = 0;
+    
+    if len_ > 0 {
+        let true_len = if len_ >= 0x80 {
+            128
+        } else {
+            len_
+        };
+        
+        if true_len > 0 {
+            let mut i = 0;
+            let mut j: u8 = 0;
+            
+            while i < s.len() {
+                let iter_ = s[i];
+                i += 1;
+                j = j.wrapping_add(1);
+                
+                let v11 = (iter_ as u32).wrapping_add(38400) & 0xffffffff;
+                let v12 = iter_ ^ ((v3 & 0xff00) >> 8) as u8;
+                v3 = v3.wrapping_add(v11).wrapping_add(163) & 0xffffffff;
+                
+                if j == 0 {
+                    v3 = 904;
+                }
+                
+                if cnt < out.len() {
+                    out[cnt] = v12;
+                    cnt += 1;
+                }
+            }
+        }
+    }
+    
+    out
+}
+
+fn decompress_zlib(data: &[u8]) -> io::Result<Vec<u8>> {
+    let mut decoder = ZlibDecoder::new(data);
+    let mut decompressed = Vec::new();
+    decoder.read_to_end(&mut decompressed)?;
+
+    Ok(decompressed)
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("sddl_dec Tool Version 4.0");
+    let args = Args::parse();
+
+    let file_path = args.input_file;
+    println!("Input file: {}", file_path);
+
+    let output_path = args.output_folder;
+    println!("Output folder: {}", output_path);
+
+    let verbose = args.verbose;
+    let keep_txt = args.keep_txt;
+
+    let file = File::open(file_path)?;
+    let file_size = file.metadata()?.len();
+    //println!("File size: {:?}", file_size);
+
+    let key = [
+        0x26, 0xE0, 0x96, 0xD3, 0xEF, 0x8A, 0x8F, 0xBB,
+        0xAA, 0x5E, 0x51, 0x6F, 0x77, 0x26, 0xC2, 0x2C,
+    ];
+    
+    let iv = [
+        0x3E, 0x4A, 0xE2, 0x3A, 0x69, 0xDB, 0x81, 0x54,
+        0xCD, 0x88, 0x38, 0xC4, 0xB9, 0x0C, 0x76, 0x66,
+    ];
+
+    let mut offset = 32;
+
+    while offset < file_size {
+        let header = read_file(&file, offset, 32)?;
+        let decrypted_header: Vec<u8>; 
+
+        match decrypt(&header, &key, &iv) {
+            Ok(v) => decrypted_header = v,
+            Err(_) => {
+                // SDDL files can have a footer(signature?) of 0x80 OR 0x100 lenght in later ones, and there is no good way to detect it before entering the while loop and the footer has no common header.
+                // so we can assume if a file fails to decode at negative offsets 0x80 or 0x100, that is the footer and it can be skipped.
+                if offset == file_size - 128 {
+                    if verbose{println!{"Found footer at negative 0x80!"};};
+                    break
+                } else if offset == file_size - 256{
+                    if verbose{println!{"Found footer at negative 0x100!"};};
+                    break
+                } else {
+                    println!{"!!Decryption error!! This file is invalid or not compatible!"};
+                    std::process::exit(0)
+                }
+            },
+        }
+
+        let decrypted_string = String::from_utf8_lossy(&decrypted_header);
+
+        let filename = decrypted_string.split("\0").next().unwrap();
+        let size_str = &decrypted_string[decrypted_string.len() - 12..];
+        let size: u64 = size_str.parse().unwrap();
+
+        println!("File: {}, Size: {}", filename, size);
+        
+        offset += 32;
+
+        let data = read_file(&file, offset, size.try_into().unwrap())?;
+        let decrypted_data = decrypt(&data, &key, &iv)?;
+
+        if decrypted_data.starts_with(&[0x11, 0x22, 0x33, 0x44]) && filename != "SDIT.FDI"{ // header of obfuscated file, SDIT.FDI also has this header but seems to work differently so its skipped
+            if verbose {
+                println!("Obfuscated file header:");
+                println!("- ID: {}", decrypted_data[5]);
+                if 8 <= decrypted_data[5] && decrypted_data[5] <= 20 {
+                    println!("-- Year guess: {}", decrypted_data[5] as u32 + 2003);
+                }
+                println!("- Base version?: {:02x?}", &decrypted_data[16..20]);
+                println!("- Old version: {:02x?}", &decrypted_data[20..24]);
+                println!("- Version: {:02x?}", &decrypted_data[24..28]);
+                println!("1. Block index: {}", u16::from_be_bytes([decrypted_data[32], decrypted_data[33]]));
+                println!("2. Control bytes: {:02x?}", &decrypted_data[34..36]);
+                println!("3. Compressed size: {}", u32::from_be_bytes([decrypted_data[36], decrypted_data[37], decrypted_data[38], decrypted_data[39]]));
+                println!("4. Uncompressed size: {}", u32::from_be_bytes([decrypted_data[40], decrypted_data[41], decrypted_data[42], decrypted_data[43]]));
+                println!("5. Checksum: {:02x?}", &decrypted_data[44..48]);
+            }
+
+            println!("- Version: {}.{}{}{}", decrypted_data[24], decrypted_data[25], decrypted_data[26], decrypted_data[27]);
+            println!("- Deciphering...");
+            let deciphered_data = decipher(&decrypted_data[48..], decrypted_data[48..].len());
+
+            let control_byte = decrypted_data[34];
+            let out_data: Vec<u8>; 
+            if verbose{println!("Control byte: {}", control_byte);};
+            if control_byte == 3 {
+                println!("-- Decompressing...");
+                out_data = decompress_zlib(&deciphered_data)?;
+            } else {
+                println!("-- Uncompressed...");
+                out_data = deciphered_data;
+            }
+
+            let first_byte;
+            if out_data[1] & 0xF0 == 0xD0 {
+                if verbose{println!("pre2013 detected!");};
+                first_byte = out_data[1] & 0x0F;
+            } else {
+                if verbose{println!("2014+ detected!");};
+                first_byte = out_data[1];
+            }
+
+            if verbose{println!("Content header:")};
+
+            let dest_offset = u32::from_be_bytes([first_byte, out_data[2], out_data[3], out_data[4]]);
+            if verbose{println!("1. Dest offset: {}", dest_offset);};
+
+            let source_offset = u32::from_be_bytes([0x00, out_data[6], out_data[7], out_data[8]]);
+            if verbose{println!("2. Source offset: {}", source_offset);};
+
+            if verbose{println!("3. Size: {}", u32::from_be_bytes([out_data[9], out_data[10], out_data[11], out_data[12]]));};
+
+            let path: PathBuf; 
+            let msg: &str;
+
+            if filename.starts_with("PEAKS.F") {
+                if source_offset == 270 {   //unique for 2014-2018 files
+                    if verbose{println!{"2014-2018 detected!"}}
+                    let embedded_file_name_string = String::from_utf8_lossy(&out_data[14..270]);
+                    let embedded_file_name = embedded_file_name_string.split("\0").next().unwrap();
+                    println!("--- Embedded file: {}", embedded_file_name);
+    
+                    let folder_path = Path::new(&output_path).join("PEAKS");
+                    fs::create_dir_all(&folder_path)?;
+                    path = Path::new(&folder_path).join(embedded_file_name);
+                    msg = "to PEAKS";
+                } else {
+                    path = Path::new(&output_path).join("PEAKS.bin");
+                    msg = "to PEAKS.bin";
+                }            
+            } else {
+                path = Path::new(&output_path).join(filename);
+                msg = "file";
+            }
+
+            fs::create_dir_all(&output_path)?;
+            let mut file = OpenOptions::new()
+                .read(true)
+                .write(true)
+                .create(true)
+                .open(path)?;
+    
+            file.seek(SeekFrom::Start(dest_offset as u64))?;
+            file.write_all(&out_data[source_offset as usize..])?;
+            println!("--- Saved {}!", msg);
+
+        } else {
+            let out_data = decrypted_data;
+            if filename.ends_with(".TXT") && !keep_txt {
+                println!("{}", String::from_utf8_lossy(&out_data));
+            } else {
+                let path = Path::new(&output_path).join(filename);
+
+                fs::create_dir_all(&output_path)?;
+                let mut file = OpenOptions::new()
+                    .write(true)
+                    .create(true)
+                    .open(path)?;
+  
+                file.write_all(&out_data)?;
+                println!("-- Saved file!");
+            } 
+        }
+
+        println!();
+        offset += size;
+    }
+
+    println!("Done! Extracted files saved to {}", output_path);
+
+    Ok(())
+}
